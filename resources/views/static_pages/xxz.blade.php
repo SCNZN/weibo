8273f4bf01613d83f4a52c000882df4939b1ab11
xianzhong <scnzn@outlook.com>
2020-04-26 06:37:21 +0000

    增加 CSRF 验证

--- resources/views/static_pages/xxz.blade.php
+++ resources/views/static_pages/xxz.blade.php
@@ -20,3 +20,43 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
+
+@extends('layouts.default')
+@section('title', '注册')
+
+@section('content')
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>注册</h5>
+    </div>
+    <div class="card-body">
+      <form method="POST" action="{{ route('users.store') }}">
+        {{ csrf_field() }}
+
+        <div class="form-group">
+          <label for="name">名称：</label>
+          <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="email">邮箱：</label>
+          <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password">密码：</label>
+          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password_confirmation">确认密码：</label>
+          <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">注册</button>
+      </form>
+    </div>
+  </div>
+</div>
+@stop
