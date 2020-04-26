c2ee6756254776cf9ff5ad6268c8cee19870f559
xianzhong <scnzn@outlook.com>
2020-04-26 04:56:22 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
+
+
 
 // xxz的测试
 Route::get('/xxz', 'StaticPagesController@xxz')->name('xxz');
